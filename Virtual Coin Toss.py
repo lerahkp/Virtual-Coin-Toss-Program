04651be671844b597b54e7be5b57db8f07d238cf
lerahkp <52873528+lerahkp@users.noreply.github.com>
2021-03-18 09:29:02 +0545

    Add files via upload

---
+++ Virtual Coin Toss.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import random
+n = random.randint(0,1)
+if n == 0:
+    print("Tails")
+else:
+    print("Heads")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
